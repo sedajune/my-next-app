b2dfa384f297d3a6f45228f262d411e3d998cc6b
Silke Groenke <silkigr@gmail.com>
2022-01-12 15:09:03 +0100

    add new pages

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,8 @@
-import {useRouter} from "next/router";
-
+import { useRouter } from "next/router";
 
 export default function Home() {
-    const router = useRouter()
-    console.log(router.query.slug)
-    
-    return (
-      <div>
-        my blog page {router.query.slug}
-        </div>
-  }
-  
+  const router = useRouter();
+  console.log(router.query.slug);
+
+  return <div>my blog page {router.query.slug}</div>;
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,3 @@
-
 export default function Home() {
-    return (
-      <div>
-        my blog overview
-        </div>
-  }
-  
+  return <div>my blog overview</div>;
+}
