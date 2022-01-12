42f4b63aa8e2c00f0db42135578a815ed93ac239
Silke Groenke <silkigr@gmail.com>
2022-01-12 15:00:41 +0100

    new changes

---
+++ pages/blog/[slug].js
@@ -0,0 +1,13 @@
+import {useRouter} from "next/router";
+
+
+export default function Home() {
+    const router = useRouter()
+    console.log(router.query.slug)
+    
+    return (
+      <div>
+        my blog page {router.query.slug}
+        </div>
+  }
+  
